cf74b37f1ca8a9db8eae8acda5a1925a5949e08b
shivani <shreeshivani24@gmail.com>
2026-08-31 21:04:31 +0530

    day4-1 c program

---
+++ day4-1.c
@@ -0,0 +1,19 @@
+//Day 4 - Q1
+//Write a program to swap two numbers without using a third variable.
+#include <stdio.h>
+
+int main()
+ {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: %d %d\n", a, b);
+
+    return 0;
+}
